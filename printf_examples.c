fd0bba714664248e8c2b8906cf020c1ff02389d9
Pierre-alban Waters <pwaters@c1r6p14.hive.fi>
2022-01-26 14:24:38 +0200

    test

--- printf_examples.c
+++ printf_examples.c
@@ -6,7 +6,7 @@
 /*   By: pwaters <pwaters@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 09:47:17 by pwaters           #+#    #+#             */
-/*   Updated: 2022/01/26 09:48:58 by pwaters          ###   ########.fr       */
+/*   Updated: 2022/01/26 10:11:57 by pwaters          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int	main(void)
 {
-	printf("%s", "Test printf");
+	char	*str;
+	int		i;
+
+	i = 10;
+	str = "Test str";
+	printf("1st var - string with s: %s; 2d var in same printf - int with d: %d", str, i);
 	return (0);
 }
